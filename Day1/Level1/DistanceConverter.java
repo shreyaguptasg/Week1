42d37ba4c1712b965c78c519f6ee4a167ea91c68
Shreya Gupta <95300301+shreyaguptasg@users.noreply.github.com>
2025-01-10 10:08:49 +0000

    .java files blunder

--- Day1/Level1/DistanceConverter.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-class DistanceConverter{
-public static void main(String [] args){
-double distanceInKilometers = 10.8;
-double distanceInMiles = distanceInKilometers*1.6;
-System.out.println("The distance " + distanceInKilometers + " km in miles is " +distanceInMiles);
-}
-}
--- Day1/Level1/VolumeOfEarth.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class VolumeOfEarth{
-public static void main(String [] args){
-int radius = 6378; //created a variable and value is assigned to it
-double volumeInKilometers = (4/3) *3.14*radius*radius*radius;// created a variable and its volume is calculated
-double volumeInMiles = volumeInKilometers*1.6;
-System.out.println("The volume of earth in cubic kilometres is " + volumeInKilometers + " and cubic miles is " +volumeInMiles);
-}
-}
